6d7defbce86d55cce9e70560d84dba9059aff3b1
LwRuan <lws.ruan@gmail.com>
2021-07-19 23:43:40 +0800

    start again

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "vkapp.h"
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main(){
+    std::cout << "hello world" << std::endl;
+    std::cout << Eigen::Vector3d::Zero() << std::endl;
+    VkPhysicalDevice _physicalDevice;
+    return 0;
+}
